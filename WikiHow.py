0b856673802f059600e1a9a93594233a2b24398a
Aja Brofferio <aja.brofferio@gmail.com>
2016-04-27 10:21:50 -0700

    Update WikiHow.py
    Updated print statements and modified inputs by changing them to raw_input

--- WikiHow.py
+++ WikiHow.py
@@ -1,10 +1,10 @@
 def howOld():
     print("Let's see how long you have lived in days, minutes and seconds!")
-    name = input("name: ")
-    print("Great,", name, ". Now go ahead and enter your age.")
-    age = int(input("age: "))
+    name = raw_input("name: ")
+    print("Great," +name+ ". Now go ahead and enter your age.")
+    age = int(raw_input("age: "))
     days = age * 365
     minutes = age * 852948
     seconds = age * 31556926
-    print (name, "has been alive for a shocking ", days, " days ", minutes, " minutes and ", seconds, " seconds! Holy Toledo!")
+    print (name+ "has been alive for a shocking "+ str(days)+ " days " +str(minutes)+ " minutes and "+ str(seconds)+ " seconds! Holy Toledo!")
 howOld()
